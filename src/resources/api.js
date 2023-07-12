7529cf29f633534e2d307511e91c310f43fd2451
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-12 09:35:53 -0300

    fix: api url and handling error 404

--- src/resources/api.js
+++ src/resources/api.js
@@ -5,16 +5,25 @@ const params = {
   },
 };
 
-const URL = 'http://localhost:3000/';
+const URL = 'http://localhost:3000/api';
 
 function getNews(subject) {
   return fetch(`${URL}/${subject}`, params)
     .then((response) => response.json())
     .catch((err) => {
-      console.error('Occoreu um error', err);
+      console.error('Ocorreu um error', err);
+    });
+}
+
+function getNewsById(subject, id) {
+  return fetch(`${URL}/${subject}/${id}`, params)
+    .then((response) => response.json())
+    .catch((err) => {
+      console.error('Ocorreu um error', err);
     });
 }
 
 export default {
   getNews,
+  getNewsById,
 };
