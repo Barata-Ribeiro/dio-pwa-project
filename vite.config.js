9be6b694ddcda62b7b3c0af7b710718f20e30374
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-12 07:07:59 -0300

    build: add pwa vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
     eslint({
       cache: false,
       fix: true,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
     }),
   ],
 });
